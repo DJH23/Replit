4170e31b45325d8b88833e38c8b49b5cc73e0090
Darren <111098562+DJH23@users.noreply.github.com>
2023-01-16 22:25:12 +0000

    added Mobile class

--- index.js
+++ index.js
@@ -1,107 +1,85 @@
-class Room {
-  constructor(x, y, height, width) {
-    this.x = x;
-    this.y = y;
-    this.height = height;
-    this.width = width
-    this.structures = []
-  }
-  remove() {
-
-  }
-  clone() {
-
-  }
-}
-
-class Structure {
-  constructor(room) {
-    this.room = room
-  }
-}
-
-class Wall extends Structure {
-  constructor(insideColour, outsideColour) {
-    this.insideColour = insideColour;
-    this.outsideColour = outsideColour;
-  }
-}
-
-class Window extends Structure {
-  constructor(opacity, isOpen) {
-    this.opacity = opacity;
-    this.isOpen = isOpen;
-  }
-  close() {
-    this.isOpen = false;
-  }
-  open() {
-    this.isOpen = true;
-  }
-  // isOpen() {
-  //getter
-  // }
-}
-
-class Furniture {
-  #height;
-
-  constructor(height, width, colour, type) {
-    this.#height = height;
-    this.width = width;
-    this.colour = colour;
-    this.type = type;
-  }
-  // getters and setters
-  getType() {
-    return this.type;
+class Mobile {
+  constructor(accType, device, number) {
+    this._accType = accType;
+    this._device = device;
+    this._number = number;
+    this._balance = 0.0;
+    this.CALL_COST = 0.245;
+    this.TEXT_COST = 0.078;
   }
 
-  // Encapsulation
-  setHeight(newHeight) {
-    if (newHeight > 3) {
-      return false;
+  // Getter for account type
+  get accType() {
+    return this._accType;
+  }
+  // Setter for account type
+  set accType(accType) {
+    this._accType = accType;
+  }
+  // Getter for device
+  get device() {
+    return this._device;
+  }
+  // Setter for device
+  set device(device) {
+    this._device = device;
+  }
+  // Getter for number
+  get number() {
+    return this._number;
+  }
+  // Setter for number
+  set number(number) {
+    this._number = number;
+  }
+  // Getter for balance
+  get balance() {
+    return this._balance.toFixed(2);
+  }
+  // Setter for balance
+  set balance(balance) {
+    this._balance = balance;
+  }
+
+//Methods
+
+  // Add credit function
+  addCredit(amount) {
+    this._balance += amount;
+    console.log("€" + amount + " added successfully. New balance: €" + this._balance.toFixed(2));
+  }
+
+  // Make call function
+  makeCall(minutes) {
+    if (this._balance < this.CALL_COST * minutes) {
+      console.log("Insufficient credit to make call. Please add more credit.");
+    } else {
+      this._balance -= this.CALL_COST * minutes;
+      console.log("Call made. New balance: €" + this._balance.toFixed(2));
     }
-    this.#height = newHeight;
-    return true;
   }
 
-  getHeight() {
-    return this.#height;
-  }
-
-}
-
-class Couch extends Furniture{
-  constructor(type, material, numSeats){
-    super();
-    this.type = type;
-    this.material = material;
-    this._numSeats = numSeats;
-  }
-  get numSeats(){
-    return this._numSeats;
-  }
-  set numSeats(numSeats){
-    if(numSeats < 2){
-      throw new Error('numSeats must be greater than one to be a couch');
+  // Send text function
+   sendText() {
+    if (this._balance < this.TEXT_COST) {
+      console.log("Insufficient credit to send text. Please add more credit.");
+    } else {
+      this._balance -= this.TEXT_COST;
+      console.log("Text sent. New balance: €" + this._balance.toFixed(2));
     }
-    this._numSeats = numSeats;
   }
 }
 
-var c1 = new Couch("Chaise Lounge", "fabric", 3);
-console.log(c1.numSeats);
-c1.numSeats = 1;
-console.log(c1.numSeats);
+let jimMobile = new Mobile("Monthly", "Samsung Galaxy S6", "0771222334")
+console.log("Account Type: " + jimMobile.accType + "\nMobile Number: " + jimMobile.number + "\nDevice: " + jimMobile.device + "\nBalance: €" + jimMobile.balance);
 
+jimMobile.accType = "PAYG";
+jimMobile.device = "iPhone 6S";
+jimMobile.number = "07713334466";
+jimMobile.balance = 15.50;
 
+console.log("Account Type: " + jimMobile.accType + "\nMobile Number: " + jimMobile.number + "\nDevice: " + jimMobile.device + "\nBalance: €" + jimMobile.balance);
 
-
-
-
-
-
-
-
-
+console.log(jimMobile.addCredit(10.0));
+console.log(jimMobile.makeCall(5));
+console.log(jimMobile.sendText(2));
---
+++ index2.js
@@ -0,0 +1,116 @@
+
+
+
+
+
+
+
+
+
+// class Room {
+//   constructor(x, y, height, width) {
+//     this.x = x;
+//     this.y = y;
+//     this.height = height;
+//     this.width = width
+//     this.structures = []
+//   }
+//   remove() {
+
+//   }
+//   clone() {
+
+//   }
+// }
+
+// class Structure {
+//   constructor(room) {
+//     this.room = room
+//   }
+// }
+
+// class Wall extends Structure {
+//   constructor(insideColour, outsideColour) {
+//     this.insideColour = insideColour;
+//     this.outsideColour = outsideColour;
+//   }
+// }
+
+// class Window extends Structure {
+//   constructor(opacity, isOpen) {
+//     this.opacity = opacity;
+//     this.isOpen = isOpen;
+//   }
+//   close() {
+//     this.isOpen = false;
+//   }
+//   open() {
+//     this.isOpen = true;
+//   }
+//   // isOpen() {
+//   //getter
+//   // }
+// }
+
+// class Furniture {
+//   #height;
+
+//   constructor(height, width, colour, type) {
+//     this.#height = height;
+//     this.width = width;
+//     this.colour = colour;
+//     this.type = type;
+//   }
+//   // getters and setters
+//   getType() {
+//     return this.type;
+//   }
+
+//   // Encapsulation
+//   setHeight(newHeight) {
+//     if (newHeight > 3) {
+//       return false;
+//     }
+//     this.#height = newHeight;
+//     return true;
+//   }
+
+//   getHeight() {
+//     return this.#height;
+//   }
+
+// }
+
+// class Couch extends Furniture{
+//   constructor(type, material, numSeats){
+//     super();
+//     this.type = type;
+//     this.material = material;
+//     this._numSeats = numSeats;
+//   }
+//   get numSeats(){
+//     return this._numSeats;
+//   }
+//   set numSeats(numSeats){
+//     if(numSeats < 2){
+//       throw new Error('numSeats must be greater than one to be a couch');
+//     }
+//     this._numSeats = numSeats;
+//   }
+// }
+
+// var c1 = new Couch("Chaise Lounge", "fabric", 3);
+// console.log(c1.numSeats);
+// c1.numSeats = 1;
+// console.log(c1.numSeats);
+
+
+
+
+
+
+
+
+
+
+
